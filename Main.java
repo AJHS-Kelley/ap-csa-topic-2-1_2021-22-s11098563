1471ee802ce0980e9b785b9d826aa49a9d5d2c04
Cook <s11098563@students.duvalschools.org>
2021-10-28 09:31:22 -0400

    v0.1
    Created 2 public classes with 2 attributes each.

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
-// Connor Cook, AP CSA Topic 2-1, 10-26-2021, 9:02am, v0.0
+// Connor Cook, AP CSA Topic 2-1, 10-28-2021, 9:30am, v0.1
 
 
 public class Main {
+    public static void main(String[] args){
+        
+    }
 
+public class ClassExample0 {
+    private int ExampleInt0;
+    private double ExampleDouble0;
+
+    }
+
+public class ClassExample1 {
+    private int ExampleInt1;
+    private double ExampleDouble1;
+    }
 }
